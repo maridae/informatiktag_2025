4471c642b0e3c9d55191d07aace3401c8c1b2763
Mareike Daeglau <Mareike.daeglau@uni-oldenburg.de>
2025-06-24 08:11:36 +0200

    add goatcounter

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 // Development-Modus erkennen
 const isDevelopment = location.hostname === 'localhost' || location.hostname === '127.0.0.1' || location.port;
 
-const CACHE_NAME = isDevelopment ? 'informatiktag-dev' : 'informatiktag-v1.0.1';
-const STATIC_CACHE = isDevelopment ? 'informatiktag-static-dev' : 'informatiktag-static-v1';
-const API_CACHE = isDevelopment ? 'informatiktag-api-dev' : 'informatiktag-api-v1';
+const CACHE_NAME = isDevelopment ? 'informatiktag-dev' : 'informatiktag-v1.0.2';
+const STATIC_CACHE = isDevelopment ? 'informatiktag-static-dev' : 'informatiktag-static-v2';
+const API_CACHE = isDevelopment ? 'informatiktag-api-dev' : 'informatiktag-api-v2';
 
 // Assets die immer gecacht werden sollen
 const STATIC_ASSETS = [
